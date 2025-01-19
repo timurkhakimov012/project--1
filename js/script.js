ad5d2db6055e8e01b4783fad83a0914655434d7e
Timerkhan_Khakimov <149237295+timurkhakimov012@users.noreply.github.com>
2025-01-19 17:25:28 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,37 +8,77 @@ const swiperWrapper = document.querySelector('.swiper-wrapper'),
       hotButton = document.querySelector('.hot__button');
 
 let currentIndex = 0;
+let startX = 0; 
+let currentX = 0; 
+let isSwiping = false;
 
 //slider
 function slide(index) {
     slides.forEach((slide, i) => {
         slide.style.display = i === index ? 'block' : 'none';
     });
+    updateIndicators(index); 
 }
 
 prevButton.addEventListener('click', () => {
     currentIndex = (currentIndex - 1 + slides.length) % slides.length;
     slide(currentIndex);
+    updateIndicators(currentIndex);
 });
-
+ 
 nextButton.addEventListener('click', () => {
     currentIndex = (currentIndex + 1) % slides.length;
     slide(currentIndex);
+    updateIndicators(currentIndex); 
 });
-
 slide(currentIndex);
 
 //Modal Sophine
 setTimeout(() => {
-    winked.classList.add('show');
-    
-    setTimeout(() => {
-        winked.classList.remove('show');
-    }, 3000); 
-}, 5000); 
+    winked.classList.add('show'); 
+}, 5000);
 
 
-//hot
-hotButton.addEventListener('click', () => {
-    hot.classList.add('hidden'); 
+//+18 section 
+// hotButton.addEventListener('click', () => {
+//     hot.classList.add('hidden'); 
+// });
+
+//indicators
+function updateIndicators(index) {
+    indicators.forEach((indicator, i) => {
+        indicator.style.backgroundColor = i === index ? 'rgba(255, 255, 255)' : '#ccc';
+    });
+};
+
+//swiper mobile
+function handleSwipe() {
+    let diff = currentX - startX;
+    if (Math.abs(diff) > 50) {
+        if (diff > 0) {
+            currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+        } else {
+            currentIndex = (currentIndex + 1) % slides.length;
+        }
+        slide(currentIndex);
+        updateIndicators(currentIndex);
+    }
+}
+
+swiperWrapper.addEventListener('touchstart', (e) => {
+    startX = e.touches[0].clientX;
+    isSwiping = true;
+});
+
+swiperWrapper.addEventListener('touchmove', (e) => {
+    if (!isSwiping) return;
+    event.preventDefault(); 
+    currentX = e.touches[0].clientX;
+});
+
+swiperWrapper.addEventListener('touchend', () => {
+    if (isSwiping) {
+        handleSwipe();
+        isSwiping = false;
+    }
 });
