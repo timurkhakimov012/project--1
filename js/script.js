b4dd2d5e402f39f022ac2ca1c1970ca9ed9757a3
Timerkhan_Khakimov <149237295+timurkhakimov012@users.noreply.github.com>
2025-01-26 13:30:41 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-// const { default: Swiper } = require("swiper");
-
 window.onload = function () {
   const swiper = new Swiper(".swiper", {
     loop: true,
@@ -7,90 +5,76 @@ window.onload = function () {
       delay: 5000,
       disableOnInteraction: false,
     },
+    navigation: {
+      nextEl: '.next',
+      prevEl: '.prev'
+    },
+    on: {
+      init: function() {
+        updateIndicators(this.realIndex);
+      },
+      slideChange: function() {
+        updateIndicators(this.realIndex);
+      }
+    }
+  });
+
+  const prevButton = document.querySelector(".prev");
+  const nextButton = document.querySelector(".next");
+
+  prevButton.addEventListener('click', () => {
+    swiper.slidePrev();
+    
+  });
+
+  nextButton.addEventListener('click', () => {
+    swiper.slideNext(); 
   });
 };
 
-// const swiperWrapper = document.querySelector(".swiper-wrapper"),
-//   slides = document.querySelectorAll(".swiper-slide"),
-//   prevButton = document.querySelector(".prev"),
-//   nextButton = document.querySelector(".next"),
-//   indicators = document.querySelectorAll(".carousel__indicators-item"),
-//   winked = document.querySelector(".winked"),
-//   hot = document.querySelector(".hot"),
-//   hotButton = document.querySelector(".hot__button");
+// //indicators
+const swiperWrapper = document.querySelector(".swiper-wrapper");
+const indicators = document.querySelectorAll(".carousel__indicators-item");
+function updateIndicators(index) {
+  indicators.forEach((indicator, i) => {
+    indicator.style.backgroundColor = 
+      i === index ? "rgba(255, 255, 255, 1)" : "rgba(255, 255, 255, 0.4)";
+  });
+}
 
-// let currentIndex = 0;
-// let startX = 0;
-// let currentX = 0;
-// let isSwiping = false;
 
-// //slider
-// function slide(index) {
-//   slides.forEach((slide, i) => {
-//     slide.style.display = i === index ? "block" : "none";
-//   });
-//   updateIndicators(index);
-// }
+//Фон для изображений
+document.addEventListener('DOMContentLoaded', () => {
+    const girlImages = document.querySelectorAll('.girl__img');
+    
+    girlImages.forEach(container => {
+      const img = container.querySelector('img');
+      if (img) {
+          const imgPath = img.getAttribute('src');
+          
+          container.classList.add('girl-container'); 
+          container.style.backgroundImage = `url('${imgPath}')`;
 
-// prevButton.addEventListener("click", () => {
-//   currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-//   slide(currentIndex);
-//   updateIndicators(currentIndex);
-// });
+            const overlay = document.createElement('div');
+            overlay.classList.add('overlay-bg'); 
+            container.appendChild(overlay);
+            
+            container.classList.add('container'); 
+            img.classList.add('image'); 
+        }
+    });
+});
 
-// nextButton.addEventListener("click", () => {
-//   currentIndex = (currentIndex + 1) % slides.length;
-//   slide(currentIndex);
-//   updateIndicators(currentIndex);
-// });
-// slide(currentIndex);
-
-// //Modal Sophine
-// setTimeout(() => {
-//   winked.classList.add("show");
-// }, 5000);
+// Sophine вам подмегнула 
+  const winked = document.querySelector('.winked');
+    setTimeout(() => {
+      winked.classList.add("show");
+    }, 5000)
 
 // //+18 section
-// // hotButton.addEventListener('click', () => {
-// //     hot.classList.add('hidden');
-// // });
-
-// //indicators
-// function updateIndicators(index) {
-//   indicators.forEach((indicator, i) => {
-//     indicator.style.backgroundColor =
-//       i === index ? "rgba(255, 255, 255)" : "#ccc";
-//   });
-// }
-
-// //swiper mobile
-// function handleSwipe() {
-//   let diff = currentX - startX;
-//   if (Math.abs(diff) > 50) {
-//     if (diff > 0) {
-//       currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-//     } else {
-//       currentIndex = (currentIndex + 1) % slides.length;
-//     }
-//     slide(currentIndex);
-//     updateIndicators(currentIndex);
-//   }
-// }
-
-// swiperWrapper.addEventListener("touchstart", (e) => {
-//   startX = e.touches[0].clientX;
-//   isSwiping = true;
+// const hot = document.querySelector(".hot");
+// const hotButton = document.querySelector(".hot__button");
+// hotButton.addEventListener('click', () => {
+//     hot.classList.add('hidden');
 // });
 
-// swiperWrapper.addEventListener("touchmove", (e) => {
-//   if (!isSwiping) return;
-//   event.preventDefault();
-//   currentX = e.touches[0].clientX;
-// });
-
-// swiperWrapper.addEventListener("touchend", () => {
-//   if (isSwiping) {
-//     handleSwipe();
-//     isSwiping = false;
-//   }
-// });
